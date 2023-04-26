eda30d77da1c5ea55d90576743f3db468951619a
ilya <zapaska96@gmail.com>
2023-04-26 10:20:33 +0500

    выводим резюме

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
-import LinkIcon from '../assets/link.svg';
+import React, { useEffect, useState } from 'react';
 import { articleApi } from '../services/articleApi';
+
+import LinkIcon from '../assets/link.svg';
+import CopyIcon from '../assets/copy.svg';
+import LoaderIcon from '../assets/loader.svg';
 
 type ArticleType = {
     url: string;
@@ -15,7 +18,9 @@ const Demo = () => {
         url: '',
     });
 
-    const [trigger, result, lastPromiseInfo] =
+    const [allArticles, setAllArticles] = useState<ArticleType[]>([]);
+
+    const [trigger, { error, isFetching }] =
         articleApi.useLazyGetSummaryQuery();
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -25,18 +30,34 @@ const Demo = () => {
 
         if (data?.summary) {
             const newArticle = { ...article, summary: data.summary };
-            setArticle(newArticle);
 
-            console.log(newArticle);
+            const updatedArticles = [newArticle, ...allArticles];
+
+            setArticle(newArticle);
+            setAllArticles(updatedArticles);
+
+            localStorage.setItem('articles', JSON.stringify(updatedArticles));
         }
+
+        // setArticle((prev) => ({ ...prev, url: '' }));
     };
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setArticle((prev) => ({ ...prev, url: e.target.value }));
     };
 
+    useEffect(() => {
+        const articlesFromLocalStorage = JSON.parse(
+            localStorage.getItem('articles') || '[]'
+        ) as ArticleType[];
+
+        if (articlesFromLocalStorage.length) {
+            setAllArticles(articlesFromLocalStorage);
+        }
+    }, []);
+
     return (
-        <section className='mt-16 w-full max-w-xl'>
+        <section className='my-16 w-full max-w-xl'>
             <div className='flex w-full flex-col gap-2'>
                 <form
                     className='relative flex items-center justify-center'
@@ -62,6 +83,56 @@ const Demo = () => {
                         <p>↵</p>
                     </button>
                 </form>
+                <div className='flex max-h-60 flex-col gap-1 overflow-y-auto'>
+                    {allArticles.map((article, index) => (
+                        <div
+                            key={`link-${index}`}
+                            onClick={() => setArticle(article)}
+                            className='link_card'
+                        >
+                            <div className='copy_btn'>
+                                <img
+                                    src={CopyIcon}
+                                    alt='copy_icon'
+                                    className='h-[40%] w-[40%] object-contain'
+                                />
+                            </div>
+                            <p className='flex-1 truncate font-satoshi text-sm font-medium text-blue-700'>
+                                {article.url}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className='my-10 flex max-w-full items-center justify-center'>
+                {isFetching ? (
+                    <img
+                        src={LoaderIcon}
+                        alt='loader'
+                        className='h-20 w-20 object-contain'
+                    />
+                ) : error ? (
+                    <p className='text-center font-inter font-bold text-black'>
+                        Что-то пошло не так...
+                        <br />
+                        <span className='font-satoshi font-normal text-gray-700'>
+                            {'status' in error && error.status}
+                        </span>
+                    </p>
+                ) : (
+                    article.summary && (
+                        <div className='flex flex-col gap-3'>
+                            <h2 className='font-satoshi text-xl font-bold text-gray-600'>
+                                <span className='blue_gradient'>Резюме</span>{' '}
+                                статьи
+                            </h2>
+                            <div className='summary_box'>
+                                <p>{article.summary}</p>
+                            </div>
+                        </div>
+                    )
+                )}
             </div>
         </section>
     );
